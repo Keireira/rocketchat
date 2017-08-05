3a393dd814c65f1e057f282c1f8aed75ac511ad4
Alena Dzhukich <github@alena.moe>
2017-08-05 10:43:17 +0300

    Add stub for <Accounts />

--- app/scripts/bricks/Sidebar/view/Sidebar.jsx
+++ app/scripts/bricks/Sidebar/view/Sidebar.jsx
@@ -25,6 +25,48 @@ Sidebar.propTypes = {
 
 Sidebar.defaultProps = {
   accounts: [
+    {
+      balance: 43563.75,
+      currency: '₽',
+      number: 57890456,
+      annual: 8.5,
+      created: 1501763532,
+      history: [{
+        type: 'top_up',
+        amount: 3500.00,
+        positive: true,
+        last_cart_number: 5682,
+        carried_out: 1504763532,
+      }],
+    },
+    {
+      balance: 6582.02,
+      currency: '$',
+      number: 47830456,
+      annual: 2.5,
+      created: 1501363532,
+      history: [{
+        type: 'top_up',
+        amount: 500.00,
+        positive: true,
+        last_cart_number: 5682,
+        carried_out: 1502363532,
+      }],
+    },
+    {
+      balance: 463.21,
+      currency: '€',
+      number: 47830426,
+      annual: 1.0,
+      created: 1500763532,
+      history: [{
+        type: 'top_up',
+        amount: 660.00,
+        positive: false,
+        last_cart_number: 5682,
+        carried_out: 1500963532,
+      }],
+    },
   ],
 };
 
