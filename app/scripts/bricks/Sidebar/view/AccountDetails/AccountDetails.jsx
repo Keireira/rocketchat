0434289c6de9d1c319da1a194ce0db3a9e3baaf3
Alena Dzhukich <github@alena.moe>
2017-08-07 00:15:25 +0300

    Add draft of detailed account page

--- app/scripts/bricks/Sidebar/view/AccountDetails/AccountDetails.jsx
+++ app/scripts/bricks/Sidebar/view/AccountDetails/AccountDetails.jsx
@@ -1,16 +1,31 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import locales from 'locales';
+import { formatUNIXTimestamp, formatBalance } from 'helpers';
+
 import AccountCart from '../AccountCart';
-import { StyledAccountDetails } from './styles';
+import { MingleShareicon } from 'icons/ux';
+import {
+  StyledAccountDetails,
+  Time,
+  Icon,
+  Inner,
+  Operation,
+  HistoryTitle,
+  HistoryCart,
+  HiddenNums,
+  HistoryWrapper,
+  NegativeVal,
+  PositiveVal,
+} from './styles';
 
 class AccountDetails extends React.PureComponent {
   componentWillMount() {
     const { accountData, match } = this.props;
 
-    const numberFromProps = accountData.number;
     const numberFromURL = parseInt(match.params.productId, 10);
-    const noData = (numberFromURL !== numberFromProps);
+    const noData = (numberFromURL !== accountData.number);
 
     if (typeof this.props.getAccountData === 'function') {
       this.props.getAccountData({
@@ -21,10 +36,6 @@ class AccountDetails extends React.PureComponent {
     }
   };
 
-  componentDidMount() {
-
-  };
-
   componentWillUnmount() {
     if (typeof this.props.clearAccount === 'function') {
       this.props.clearAccount();
@@ -32,6 +43,10 @@ class AccountDetails extends React.PureComponent {
   };
 
   render() {
+    const history = this.props.accountData.history.sort((a, b) => {
+      return (a.carried_out >= b.carried_out) ? -1 : 1;
+    });
+
     return (
       <StyledAccountDetails>
         <AccountCart
@@ -39,6 +54,38 @@ class AccountDetails extends React.PureComponent {
           match={this.props.match}
           {...this.props.accountData}
         />
+
+        <HistoryTitle>{locales.operations_history}</HistoryTitle>
+        <HistoryWrapper>
+          {
+            history.map((item) => {
+              const time = formatUNIXTimestamp(item.carried_out, true);
+              const formattedAmouunt = formatBalance(item.amount);
+              const { currency } = this.props.accountData;
+              const amount = (item.positive)
+                ? <PositiveVal>{`${formattedAmouunt} ${currency}`}</PositiveVal>
+                : <NegativeVal>{`${formattedAmouunt} ${currency}`}</NegativeVal>;
+
+              return (
+                <HistoryCart key={item.carried_out / item.last_cart_number}>
+                  <Inner>
+                    <Time>{time}</Time>
+                    <Operation>
+                      <span>{locales.actions[item.type]}</span>
+                      <HiddenNums>****</HiddenNums>
+                      <span>{item.last_cart_number}</span>
+                      {amount}
+                    </Operation>
+
+                    <Icon>
+                      <MingleShareicon width={16} height={16} />
+                    </Icon>
+                  </Inner>
+                </HistoryCart>
+              );
+            })
+          }
+        </HistoryWrapper>
       </StyledAccountDetails>
     );
   };
--- app/scripts/bricks/Sidebar/view/AccountDetails/styles.js
+++ app/scripts/bricks/Sidebar/view/AccountDetails/styles.js
@@ -2,4 +2,102 @@ import styled from 'styled-components';
 
 export const StyledAccountDetails = styled.div`
   display: flex;
+  flex-direction: column;
+  height: 100%;
+`;
+
+export const HistoryTitle = styled.div`
+  font-family: 'roboto-regular';
+  font-size: 1rem;
+  line-height: 1.25rem;
+  padding: .75rem 2.5rem;
+  color: ${({ theme }) => theme.spanish_gray(1)};
+`;
+
+export const Time = styled.span`
+  display: flex;
+  width: 100%;
+`;
+
+export const HistoryCart = styled.div`
+  width: 100%;
+  padding: .75rem 2.5rem;
+
+  &:nth-of-type(odd) {
+    background-color: ${({ theme }) => theme.snow(1)};
+  }
+
+  &:hover {
+    cursor: default;
+  }
+`;
+
+export const HistoryWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  justify-content: flex-start;
+  overflow-y: scroll;
+
+  color: ${({ theme }) => theme.spanish_gray(1)};
+  font-family: 'roboto-regular';
+  line-height: 1.25rem;
+  font-size: .875rem;
+
+  width: 100%;
+`;
+
+export const NegativeVal = styled.span`
+  color: ${({ theme }) => theme.maximum_red(1)};
+
+  &::before {
+    content: '- ';
+    margin-left: .5rem;
+  }
+`;
+
+export const PositiveVal = styled.span`
+  color: ${({ theme }) => theme.harlequin_green(1)};
+
+  &::before {
+    content: '+ ';
+    margin-left: .5rem;
+  }
+`;
+
+export const HiddenNums = styled.sup`
+  font-size: 1rem;
+  margin: -.1rem .1rem 0 .2rem;
+`;
+
+export const Operation = styled.div`
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  color: ${({ theme }) => theme.raisin_black(1)};
+`;
+
+export const Icon = styled.a`
+  display: flex;
+
+  position: absolute;
+  right: -10px;
+  top: 50%;
+  transform: translateY(-50%);
+
+  padding: 10px;
+  transition: all .2s ease-in-out;
+  border-radius: 50%;
+
+  &:hover {
+    cursor: pointer;
+    background-color: ${({ theme }) => theme.gainsboro(1)};
+  }
+`;
+
+export const Inner = styled.div`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
 `;
