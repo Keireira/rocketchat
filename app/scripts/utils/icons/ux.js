eb6184ef95b2728ea59776794a1b579fa5d4b7a3
Alena Dzhukich <github@alena.moe>
2017-08-07 00:13:38 +0300

    General add
    - Add MingleShare icon
    - Add new locales
    - Change media beta
    - Fix animations export

--- app/scripts/utils/icons/ux.js
+++ app/scripts/utils/icons/ux.js
@@ -2,11 +2,16 @@ import styled from 'styled-components';
 
 import Maximize from 'img_icons/maximize.svg';
 import Close from 'img_icons/close.svg';
+import MingleShare from 'img_icons/mingle-share.svg';
 
 export const MaximizeIcon = styled(Maximize)`
   pointer-events: none;
 `;
 
 export const CloseIcon = styled(Close)`
+  pointer-events: none;
+`;
+
+export const MingleShareicon = styled(MingleShare)`
   pointer-events: none;
 `;
