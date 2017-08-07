3a2a62508b1a0a253c53017496ae5532cecd3162
Alena Dzhukich <github@alena.moe>
2017-08-07 04:17:23 +0300

    Change enter chat button

--- app/scripts/common/pages/LoginPage/LoginPage.jsx
+++ app/scripts/common/pages/LoginPage/LoginPage.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import locales from 'locales';
-import { StyledLoginPage, LoginButton } from './styles';
+
+import { StyledLoginPage, ChatNotify } from './styles';
 
 const LoginPage = () => {
   return (
     <StyledLoginPage>
-      <LoginButton to="/chat/accounts/">
-        {locales.chat_enter}
-      </LoginButton>
+      <ChatNotify to="/chat/accounts/">
+        !
+      </ChatNotify>
     </StyledLoginPage>
   );
 };
--- app/scripts/common/pages/LoginPage/styles.js
+++ app/scripts/common/pages/LoginPage/styles.js
@@ -1,34 +1,49 @@
 import styled from 'styled-components';
 import { Link } from 'react-router-dom';
+import { trambling } from 'styles/utils/animations';
 
 export const StyledLoginPage = styled.div`
-  display: flex;
-  align-items: center;
-  justify-content: center;
-
   width: 100%;
   height: 100%;
+  max-width: 1440px;
+
+  position: relative;
 
   background-color: ${({ theme }) => theme.white_smoke(1)};
 `;
 
-export const LoginButton = styled(Link)`
-  font-size: 2rem;
+export const ChatNotify = styled(Link)`
+  font-size: 4rem;
+  font-weight: bold;
   font-family: 'roboto-regular';
-  color: ${({ theme }) => theme.blue_jeans(1)};
 
   display: flex;
   align-items: center;
   justify-content: center;
 
-  padding: 2rem 4rem;
-  border-radius: .5rem;
-  border: 2px solid ${({ theme }) => theme.blue_jeans(1)};
+  position: absolute;
+  bottom: 2rem;
+  right: 2rem;
 
-  background-color: transparent;
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
   transition: all .25s ease-in-out;
 
+  animation-name: ${trambling};
+  animation-delay: 2s;
+  animation-duration: 1s;
+  animation-play-state: running;
+  animation-iteration-count: infinite;
+  animation-timing-function: ease-in-out;
+
+  background-color: transparent;
+  color: ${({ theme }) => theme.blue_jeans(1)};
+  border: 2px solid ${({ theme }) => theme.blue_jeans(1)};
+
   &:hover {
+    animation-play-state: paused;
+
     color: ${({ theme }) => theme.white_smoke(1)};
     background-color: ${({ theme }) => theme.blue_jeans(1)};
   }
