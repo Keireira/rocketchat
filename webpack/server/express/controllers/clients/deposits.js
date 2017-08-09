fc387a96196859f14d6711c9b9c7908440866287
Alena Dzhukich <github@alena.moe>
2017-08-09 03:21:38 +0300

    Add deposits controller

--- webpack/server/express/controllers/clients/deposits.js
+++ webpack/server/express/controllers/clients/deposits.js
@@ -1,6 +1,27 @@
-const stubs = [
+const { random } = require('lodash');
+const currencySymbols = ['₽', '$', '€'];
 
-];
+const createDeposit = () => {
+  const balance = random(100.00, 30000.99);
+  const percent = random(1.00, 10.99);
+
+  return {
+    balance: parseFloat(balance.toFixed(2)),
+    percent: parseFloat(percent.toFixed(2)),
+    interestMonth: parseFloat((balance * percent / 100).toFixed(2)),
+    period: 24,
+    passed: 6,
+    number: random(1111111, 9999999),
+    currency: currencySymbols[random(0, 2)],
+  };
+};
+
+const makeDepositsStub = () => {
+  const length = random(3, 10);
+  const data = new Array(length).fill(null);
+
+  return data.map(createDeposit);
+};
 
 const deposits = (res, { client_id }) => {
   if (isNaN(client_id) || client_id === -666) {
@@ -10,7 +31,7 @@ const deposits = (res, { client_id }) => {
   }
 
   return res.json({
-    deposits: stubs,
+    deposits: makeDepositsStub(),
   });
 };
 
